1cb2f11130def7651bea201891a3df42282d50fa
Max Breakwell <max@perkville.com>
2015-12-17 09:52:14 -0800

    - Add method to fetch `product` from a `line_item` - Pep8 fix

--- shopify/resources/line_item.py
+++ shopify/resources/line_item.py
@@ -1,6 +1,10 @@
 from ..base import ShopifyResource
+from .product import Product
 
 
 class LineItem(ShopifyResource):
-  class Property(ShopifyResource):
-    pass
+    class Property(ShopifyResource):
+        pass
+
+    def product(self, **kwargs):
+        return Product.find(self.product_id, **kwargs)
